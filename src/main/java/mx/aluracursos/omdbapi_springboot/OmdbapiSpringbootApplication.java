d27c5fe9ae96ad99f95100bd52dd63849f7aaf48
jmikhaelz <jmz06@outlook.com>
2025-05-27 14:26:47 -0600

    init (app) Creacion y estructura del proyecto.

---
+++ src/main/java/mx/aluracursos/omdbapi_springboot/OmdbapiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package mx.aluracursos.omdbapi_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmdbapiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmdbapiSpringbootApplication.class, args);
+	}
+
+}
