478238a51268a1e022bfeab341190dc40853bb6a
jmikhaelz <jmz06@outlook.com>
2025-05-27 18:09:46 -0600

    feat (model) : Clase de implementacion de datos sobre Series.

---
+++ src/main/java/mx/aluracursos/omdbapi_springboot/models/Serie.java
@@ -0,0 +1,13 @@
+package mx.aluracursos.omdbapi_springboot.models;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Serie(
+        @JsonAlias("Title") String titulo,
+        @JsonAlias("Genre") String genero,
+        @JsonAlias("totalSeasons") int totalTemporadas,
+        @JsonAlias("Poster") String poster,
+        @JsonAlias("imdbRating") String evaluacion) {
+}
