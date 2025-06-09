4a77674af55f797690115248b700502e6fb74f57
jmikhaelz <jmz06@outlook.com>
2025-06-08 19:55:22 -0600

    feat (client) Cambio de logica sobre peticion de URI directo para APi.

--- src/main/java/mx/aluracursos/omdbapi_springboot/client/OmdbApiClient.java
+++ src/main/java/mx/aluracursos/omdbapi_springboot/client/OmdbApiClient.java
@@ -2,24 +2,18 @@ package mx.aluracursos.omdbapi_springboot.client;
 
 import java.io.IOException;
 import java.net.URI;
-import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
 
 import org.springframework.stereotype.Component;
 
-import mx.aluracursos.omdbapi_springboot.config.OmdbApiProperties;
-
 @Component
 public class OmdbApiClient {
-    private final OmdbApiProperties config = new OmdbApiProperties();
     private final HttpClient client = HttpClient.newHttpClient();
 
-    public String getJSOString(OmdbQueryParams params) throws IOException {
+    public String getJSOString(URI uri) throws IOException {
         try {
-            URI uri = buildOmdUri(params);
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(uri)
                     .build();
@@ -32,17 +26,5 @@ public class OmdbApiClient {
         } catch (Exception e) {
             throw new IOException("Error al realizar la consulta a OMDBAPI", e);
         }
-    }
-
-    private URI buildOmdUri(OmdbQueryParams params) {
-        StringBuilder urlApi = new StringBuilder(config.getUrl());
-
-        if (params.getTitulo() != null)
-            urlApi.append(URLEncoder.encode(params.getTitulo().toLowerCase(), StandardCharsets.UTF_8));
-        if (params.getTemporada() != 0)
-            urlApi.append("&Season=").append(params.getTemporada());
-        if (params.getEpisodio() != 0)
-            urlApi.append("&Episode=").append(params.getEpisodio());
-        return URI.create(urlApi.toString());
     }
 }
