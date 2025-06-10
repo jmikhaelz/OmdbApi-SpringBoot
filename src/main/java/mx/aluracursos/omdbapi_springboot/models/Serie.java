ea3c45ac2ac889e02756abc454ad64827679bfb9
jmikhaelz <jmz06@outlook.com>
2025-06-09 19:56:09 -0600

    feat (models) Class Serie se agregaron atributos

--- src/main/java/mx/aluracursos/omdbapi_springboot/models/Serie.java
+++ src/main/java/mx/aluracursos/omdbapi_springboot/models/Serie.java
@@ -6,16 +6,17 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record Serie(
                 @JsonAlias("Title") String titulo,
-                @JsonAlias("Genre") String genero,
                 @JsonAlias("totalSeasons") int totalTemporadas,
-                @JsonAlias("Poster") String poster,
                 @JsonAlias("imdbRating") String evaluacion,
-                @JsonAlias("Released") String lanzamiento) {
+                @JsonAlias("Released") String lanzamiento, @JsonAlias("Poster") String poster,
+                @JsonAlias("Genre") String genero,
+                @JsonAlias("Actors") String actores,
+                @JsonAlias("Plot") String sinopsis) {
 
         @Override
         public String toString() {
                 return "\tSerie : " + titulo + " Genero : " + genero +
-                                "Lanzamiento : " + lanzamiento + " Evalucion : " + evaluacion;
+                                " Lanzamiento : " + lanzamiento + " Evalucion : " + evaluacion;
         }
 
 }
