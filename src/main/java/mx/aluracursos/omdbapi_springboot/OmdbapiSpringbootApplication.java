1588fec6d60a07761aac9367da6b5f7aaa1819b8
jmikhaelz <jmz06@outlook.com>
2025-06-19 21:07:18 -0600

    enable (Propieties) : Habilitacion de propiedades

--- src/main/java/mx/aluracursos/omdbapi_springboot/OmdbapiSpringbootApplication.java
+++ src/main/java/mx/aluracursos/omdbapi_springboot/OmdbapiSpringbootApplication.java
@@ -2,7 +2,9 @@ package mx.aluracursos.omdbapi_springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties
 @SpringBootApplication
 public class OmdbapiSpringbootApplication {
 
