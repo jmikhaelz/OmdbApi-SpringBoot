66737ced18674448ab9a650243d302faf1fe1bba
jmikhaelz <jmz06@outlook.com>
2025-06-23 21:45:55 -0600

    update (repository) : Consultas a DB por JPA con funciones

--- src/main/java/mx/aluracursos/omdbapi_springboot/repository/SerieRepository.java
+++ src/main/java/mx/aluracursos/omdbapi_springboot/repository/SerieRepository.java
@@ -1,8 +1,20 @@
 package mx.aluracursos.omdbapi_springboot.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import mx.aluracursos.omdbapi_springboot.models.Categoria;
 import mx.aluracursos.omdbapi_springboot.models.SerieClass;
 
 public interface SerieRepository extends JpaRepository<SerieClass, Long> {
+    Optional<SerieClass> findByTituloContainsIgnoreCase(String nombreSerie);
+
+    List<SerieClass> findTop5ByOrderByEvaluacionDesc();
+
+    List<SerieClass> findByGenero(Categoria categoria);
+
+    List<SerieClass> findByTotalTemporadasLessThanEqualAndEvaluacionGreaterThanEqual(int totalTemporadas,
+            Double evaluacion);
 }
